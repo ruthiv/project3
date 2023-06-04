1301343ec05b8577554892c58e911045107fd330
ruthiv <rvengrover@gmail.com>
2023-06-04 14:49:37 +0300

    update

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -6,32 +6,32 @@ import allCoupons from "../../../Services/AllCoupons";
 import notify from "../../../Services/NotificationService";
 import AllCouponCard from "./CouponCard/AllCouponCard";
 import "./Home.css";
-import {SiLinkedin} from "react-icons/si";
+import { SiLinkedin } from "react-icons/si";
 
 function Home(): JSX.Element {
     const [coupons, setCoupons] = useState<CouponModel[]>([]);
-    
+
     useEffect(() => {
-           allCoupons.getAllCoupons().then((res) => {
-                // notify.success("fetch coupons successfully");
-                setCoupons(res.data);
-                store.dispatch(fetchCouponAction(res.data));
-            }).catch((error) => {
-                notify.error(error);
-            })
+        allCoupons.getAllCoupons().then((res) => {
+            // notify.success("fetch coupons successfully");
+            setCoupons(res.data);
+            store.dispatch(fetchCouponAction(res.data));
+        }).catch((error) => {
+            notify.error(error);
+        })
     }, []);
 
 
 
     return (
         <div>
-        <h2>Homepage</h2>
-        <div className="Home" id="home-list-top">
-          {coupons.length > 0 ? coupons.map((coupon) => (
-           <AllCouponCard key={coupon.id} coupon={coupon} /> 
-            )) : <span>no coupons yet 😒</span>} 
-            <a href="#home-list-top">👆</a>
-        </div>
+            <h2>Homepage</h2>
+            <div className="Home" id="home-list-top">
+                {coupons.length > 0 ? coupons.map((coupon) => (
+                    <AllCouponCard key={coupon.id} coupon={coupon} />
+                )) : <span>no coupons yet 😒</span>}
+                <a href="#home-list-top">👆</a>
+            </div>
         </div>
     );
 }
